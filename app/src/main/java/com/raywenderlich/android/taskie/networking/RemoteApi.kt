aa004621edf85d721621e8c627c6d8ed7568c180
eulucasm <lucaspoletis@gmail.com>
2022-02-11 11:43:38 -0300

    create a http connection

--- app/src/main/java/com/raywenderlich/android/taskie/networking/RemoteApi.kt
+++ app/src/main/java/com/raywenderlich/android/taskie/networking/RemoteApi.kt
@@ -38,58 +38,106 @@ import com.raywenderlich.android.taskie.model.Task
 import com.raywenderlich.android.taskie.model.UserProfile
 import com.raywenderlich.android.taskie.model.request.AddTaskRequest
 import com.raywenderlich.android.taskie.model.request.UserDataRequest
-
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.StringBuilder
+import java.net.HttpURLConnection
+import java.net.URL
 
 
 const val BASE_URL = "https://taskie-rw.herokuapp.com"
 
 class RemoteApi {
 
-  fun loginUser(userDataRequest: UserDataRequest, onUserLoggedIn: (String?, Throwable?) -> Unit) {
-    onUserLoggedIn("token", null)
-  }
+   fun loginUser(userDataRequest: UserDataRequest, onUserLoggedIn: (String?, Throwable?) -> Unit) {
+      onUserLoggedIn("token", null)
+   }
 
-  fun registerUser(userDataRequest: UserDataRequest, onUserCreated: (String?, Throwable?) -> Unit) {
-    onUserCreated("Success!", null)
-  }
+   fun registerUser(
+      userDataRequest: UserDataRequest,
+      onUserCreated: (String?, Throwable?) -> Unit
+   ) {
+      Thread(Runnable {
+         val connection = URL("$BASE_URL/api/register").openConnection() as HttpURLConnection
+         connection.requestMethod = "POST"
+         connection.setRequestProperty("Content-Type", "application/json")
+         connection.setRequestProperty("Accept", "application/json")
+         connection.readTimeout = 10000
+         connection.connectTimeout = 10000
+         connection.doOutput = true
+         connection.doInput = true
 
-  fun getTasks(onTasksReceived: (List<Task>, Throwable?) -> Unit) {
-    onTasksReceived(listOf(
-        Task("id",
-            "Wash laundry",
-            "Wash the whites and colored separately!",
-            false,
-            1
-        ),
-        Task("id2",
-            "Do some work",
-            "Finish the project",
-            false,
-            3
-        )
-    ), null)
-  }
+         val body =
+            "{\"name\":\"${userDataRequest.name}\",\"email\":\"${userDataRequest.email}\"," +
+                    "\"password\":\"${userDataRequest.password}\"}"
 
-  fun deleteTask(onTaskDeleted: (Throwable?) -> Unit) {
-    onTaskDeleted(null)
-  }
+         val bytes = body.toByteArray()
 
-  fun completeTask(onTaskCompleted: (Throwable?) -> Unit) {
-    onTaskCompleted(null)
-  }
+         try {
+            connection.outputStream.use { outputStream ->
+               outputStream.write(bytes)
+            }
+            val reader = InputStreamReader(connection.inputStream)
 
-  fun addTask(addTaskRequest: AddTaskRequest, onTaskCreated: (Task?, Throwable?) -> Unit) {
-    onTaskCreated(
-        Task("id3",
+            reader.use { input ->
+               val response = StringBuilder()
+               val bufferedReader = BufferedReader(input)
+
+               bufferedReader.useLines { lines ->
+                  lines.forEach {
+                     response.append(it.trim())
+                  }
+               }
+               onUserCreated(response.toString(), null)
+            }
+         } catch (error: Throwable) {
+            onUserCreated(null, error)
+         }
+      }).start()
+   }
+
+   fun getTasks(onTasksReceived: (List<Task>, Throwable?) -> Unit) {
+      onTasksReceived(
+         listOf(
+            Task(
+               "id",
+               "Wash laundry",
+               "Wash the whites and colored separately!",
+               false,
+               1
+            ),
+            Task(
+               "id2",
+               "Do some work",
+               "Finish the project",
+               false,
+               3
+            )
+         ), null
+      )
+   }
+
+   fun deleteTask(onTaskDeleted: (Throwable?) -> Unit) {
+      onTaskDeleted(null)
+   }
+
+   fun completeTask(onTaskCompleted: (Throwable?) -> Unit) {
+      onTaskCompleted(null)
+   }
+
+   fun addTask(addTaskRequest: AddTaskRequest, onTaskCreated: (Task?, Throwable?) -> Unit) {
+      onTaskCreated(
+         Task(
+            "id3",
             addTaskRequest.title,
             addTaskRequest.content,
             false,
             addTaskRequest.taskPriority
-        ), null
-    )
-  }
+         ), null
+      )
+   }
 
-  fun getUserProfile(onUserProfileReceived: (UserProfile?, Throwable?) -> Unit) {
-    onUserProfileReceived(UserProfile("mail@mail.com", "Filip", 10), null)
-  }
+   fun getUserProfile(onUserProfileReceived: (UserProfile?, Throwable?) -> Unit) {
+      onUserProfileReceived(UserProfile("mail@mail.com", "Filip", 10), null)
+   }
 }
--- app/src/main/java/com/raywenderlich/android/taskie/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/raywenderlich/android/taskie/ui/register/RegisterActivity.kt
@@ -34,10 +34,12 @@
 
 package com.raywenderlich.android.taskie.ui.register
 
+import android.net.ConnectivityManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.raywenderlich.android.taskie.R
 import com.raywenderlich.android.taskie.model.request.UserDataRequest
+import com.raywenderlich.android.taskie.networking.NetworkStatusChecker
 import com.raywenderlich.android.taskie.networking.RemoteApi
 import com.raywenderlich.android.taskie.utils.gone
 import com.raywenderlich.android.taskie.utils.toast
@@ -47,44 +49,55 @@ import kotlinx.android.synthetic.main.activity_register.*
 /**
  * Displays the Register screen, with the options to register, or head over to Login!
  */
+
 class RegisterActivity : AppCompatActivity() {
 
-  private val remoteApi = RemoteApi()
+   private val networkStatusChecker by lazy {
+      NetworkStatusChecker(getSystemService(ConnectivityManager::class.java))
+   }
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_register)
-    initUi()
-  }
+   private val remoteApi = RemoteApi()
 
-  private fun initUi() {
-    register.setOnClickListener {
-      processData(nameInput.text.toString(), emailInput.text.toString(),
-          passwordInput.text.toString())
-    }
-  }
+   override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      setContentView(R.layout.activity_register)
+      initUi()
+   }
 
-  private fun processData(username: String, email: String, password: String) {
-    if (username.isNotBlank() && email.isNotBlank() && password.isNotBlank()) {
-      remoteApi.registerUser(UserDataRequest(email, password, username)) { message, error ->
-        if (message != null) {
-          toast(message)
-          onRegisterSuccess()
-        } else if (error != null) {
-          onRegisterError()
-        }
+   private fun initUi() {
+      register.setOnClickListener {
+         processData(
+            nameInput.text.toString(), emailInput.text.toString(),
+            passwordInput.text.toString()
+         )
       }
-    } else {
-      onRegisterError()
-    }
-  }
+   }
 
-  private fun onRegisterSuccess() {
-    errorText.gone()
-    finish()
-  }
+   private fun processData(username: String, email: String, password: String) {
+      if (username.isNotBlank() && email.isNotBlank() && password.isNotBlank()) {
+         networkStatusChecker.performIfConnectedToInternet {
+            remoteApi.registerUser(UserDataRequest(email, password, username)) { message, error ->
+               runOnUiThread {
+                  if (message != null) {
+                     toast(message)
+                     onRegisterSuccess()
+                  } else if (error != null) {
+                     onRegisterError()
+                  }
+               }
+            }
+         }
+      } else {
+         onRegisterError()
+      }
+   }
 
-  private fun onRegisterError() {
-    errorText.visible()
-  }
+   private fun onRegisterSuccess() {
+      errorText.gone()
+      finish()
+   }
+
+   private fun onRegisterError() {
+      errorText.visible()
+   }
 }
